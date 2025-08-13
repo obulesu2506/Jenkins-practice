3f614e1d1412a11ea71be8ba83006269431e95ab
obulesu <obulesu.cloud@gmail.com>
2025-08-13 17:58:24 -0400

    Automation script update

---
+++ AutomationInstance/jenkins.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "jenkins" {
+
+    ami = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    instance_type = "t3.small"
+
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp3"
+    }
+    
+    user_data = file("jenkins.sh")
+
+    tags = {
+      Name = "Jenkins"
+    }
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "allow_tls"
+    description = "Allow Inbound & outbound traffic request"
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "allow_tls_1"
+    }
+}
